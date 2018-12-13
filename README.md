d25c6dd573ffc184c777d1d706909a095e2e2896
yangshaofeng <yangshaofeng@phoenixos.com>
2018-12-13 16:02:37 +0800

    增加圆角矩形支持

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # NavigationFragment
-#### App 主要框架
+#### App 主要框架,以及常用的工具和学习记录
 - 完美解决Fragment重叠的问题
 - Activity重启后可以保留最后可见的Fragment
 - 模仿知识星球实现了滑动效果
+- 自定义下拉刷新
+- 自定义圆角图片截取图片中心
+
 ![主页](https://raw.githubusercontent.com/fewwind/img_folder/master/home_frag.png)
 
--- app/src/main/java/com/chaozhuo/parentmanager/weight/CircleImageView.java
+++ app/src/main/java/com/chaozhuo/parentmanager/weight/CircleImageView.java
@@ -1,11 +1,13 @@
 package com.chaozhuo.parentmanager.weight;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.Nullable;
@@ -19,6 +21,8 @@ import com.chaozhuo.parentmanager.R;
  */
 
 public class CircleImageView extends ImageView {
+    public static int TYPE_CIRCLE = 0;
+    public static int TYPE_ROUND = 1;
     private float width;
     private float height;
     private float radio;
@@ -26,6 +30,8 @@ public class CircleImageView extends ImageView {
     private Paint paintLine;
     private Matrix matrix;
     private int lineW = 12;
+    private int roundAngle;
+    private int type;
 
     public CircleImageView(Context context) {
         this(context, null);
@@ -41,6 +47,10 @@ public class CircleImageView extends ImageView {
         paintLine.setColor(getResources().getColor(R.color.colorPrimary));
         paintLine.setStrokeWidth(lineW);
         paintLine.setStyle(Paint.Style.STROKE);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView);
+        roundAngle = typedArray.getDimensionPixelSize(R.styleable.CircleImageView_bordeAngle, 88);
+        type = typedArray.getInt(R.styleable.CircleImageView_type, TYPE_CIRCLE);
+        typedArray.recycle();
     }
 
     @Override
@@ -60,20 +70,26 @@ public class CircleImageView extends ImageView {
             return;
         }
         Bitmap bitmap = ((BitmapDrawable) getDrawable()).getBitmap();
-        int bitmapW = Math.min(bitmap.getWidth(), bitmap.getHeight());
-        int x, y;
-        x = Math.abs(bitmap.getWidth() - bitmapW) / 2;
-        y = Math.abs(bitmap.getHeight() - bitmapW) / 2;
-        bitmap = Bitmap.createBitmap(bitmap, x, y, bitmapW, bitmapW);
-        float scale = Math.min(width / bitmap.getWidth(), height / bitmap.getHeight());
-        BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        matrix.setScale(scale, scale);
-        float a = Math.abs(width - radio * 2) / 2;
-        float b = Math.abs(height - radio * 2) / 2;
-        matrix.postTranslate(a, b);
-        shader.setLocalMatrix(matrix);
-        paint.setShader(shader);
-        canvas.drawCircle(width / 2, height / 2, radio, paint);
-        canvas.drawCircle(width / 2, height / 2, radio - lineW / 2, paintLine);
+
+        if (type == TYPE_CIRCLE) {
+            int bitmapW = Math.min(bitmap.getWidth(), bitmap.getHeight());
+            int x = Math.abs(bitmap.getWidth() - bitmapW) / 2;
+            int y = Math.abs(bitmap.getHeight() - bitmapW) / 2;
+            bitmap = Bitmap.createBitmap(bitmap, x, y, bitmapW, bitmapW);
+            float scale = Math.min(width / bitmap.getWidth(), height / bitmap.getHeight());
+            BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            matrix.setScale(scale, scale);
+            shader.setLocalMatrix(matrix);
+            float a = Math.abs(width - radio * 2) / 2;
+            float b = Math.abs(height - radio * 2) / 2;
+            matrix.postTranslate(a, b);
+            paint.setShader(shader);
+            canvas.drawCircle(width / 2, height / 2, radio, paint);
+            canvas.drawCircle(width / 2, height / 2, radio - lineW / 2, paintLine);
+        } else {
+            BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            paint.setShader(shader);
+            canvas.drawRoundRect(new RectF(0, 0, width, height), roundAngle, roundAngle, paint);
+        }
     }
 }
