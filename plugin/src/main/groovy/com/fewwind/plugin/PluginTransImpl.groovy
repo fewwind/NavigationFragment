5af6880fe3d3cbc90228dc4d28e7702f7304655a
yangshaofeng@ruzuo.cn <yangshaofeng@ruzuo.cn>
2020-10-21 10:46:17 +0800

    gradle plugin

---
+++ plugin/src/main/groovy/com/fewwind/plugin/PluginTransImpl.groovy
@@ -0,0 +1,15 @@
+package com.fewwind.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginTransImpl implements Plugin<Project> {
+    void apply(Project project) {
+//        def android = project.extensions.getByType(AppExtension)
+//        LifeCycleTransform transform = new LifeCycleTransform()
+//        android.registerTransform(transform)
+        project.task('FewwindTask') {
+            println "Hello gradle plugin"
+        }
+    }
+}
