602f874eb148e1c88ca72d6ea6ed64e37251082a
yangshaofeng@ruzuo.cn <yangshaofeng@ruzuo.cn>
2020-11-03 15:11:56 +0800

    livedata粘性事件

---
+++ app/src/main/java/com/feng/advance/copy2creat/Event/CleanLiveData.kt
@@ -0,0 +1,43 @@
+package com.feng.advance.copy2creat.Event
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import com.orhanobut.logger.Logger
+
+class CleanLiveData<T> : MutableLiveData<T>() {
+    //    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+//        super.observe(owner, object : Observer<T> {
+//            override fun onChanged(t: T?) {
+//                if (t != null) {
+//                    observer.onChanged(t)
+//                }
+//            }
+//        })
+//    }
+    override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+        super.observe(owner, ObserWapper(observer))
+    }
+
+    class ObserWapper<T>(var origin: Observer<T>, var stop: Boolean = true) : Observer<T> {
+        override fun onChanged(t: T?) {
+            if (stop) {
+                stop = false
+            } else {
+                origin.onChanged(t)
+            }
+        }
+    }
+
+//    override fun removeObserver(observer: Observer<in T>) {
+//        super.removeObserver(observer)
+//        Logger.w("remove")
+//        value = null
+//    }
+//
+//    override fun removeObservers(owner: LifecycleOwner) {
+//        super.removeObservers(owner)
+//        Logger.e("remove")
+//    }
+
+}
