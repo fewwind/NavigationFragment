54b7bc195e98f2a6307515befc2b1c36de1b5bfd
yangshaofeng@ruzuo.cn <yangshaofeng@ruzuo.cn>
2020-11-03 16:13:22 +0800

    利用包可见性禁止粘性事件

---
+++ app/src/main/java/android/arch/lifecycle/ExtLiveData.java
@@ -0,0 +1,7 @@
+package android.arch.lifecycle;
+
+public class ExtLiveData<T> extends MutableLiveData<T> {
+    @Override public int getVersion() {
+        return super.getVersion();
+    }
+}
--- app/src/main/java/com/feng/advance/copy2creat/Event/CleanLiveData.kt
+++ app/src/main/java/com/feng/advance/copy2creat/Event/CleanLiveData.kt
@@ -1,11 +1,12 @@
 package com.feng.advance.copy2creat.Event
 
+import android.arch.lifecycle.ExtLiveData
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 import com.orhanobut.logger.Logger
 
-class CleanLiveData<T> : MutableLiveData<T>() {
+class CleanLiveData<T> : ExtLiveData<T>() {
     //    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
 //        super.observe(owner, object : Observer<T> {
 //            override fun onChanged(t: T?) {
@@ -16,7 +17,7 @@ class CleanLiveData<T> : MutableLiveData<T>() {
 //        })
 //    }
     override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
-        super.observe(owner, ObserWapper(observer))
+        super.observe(owner, ObserWapper(observer,version >- 1))
     }
 
     class ObserWapper<T>(var origin: Observer<T>, var stop: Boolean = true) : Observer<T> {
