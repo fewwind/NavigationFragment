768f9beaed45ddca7fdd5bc40740698418f75116
yangshaofeng@ruzuo.cn <yangshaofeng@ruzuo.cn>
2020-11-18 16:50:09 +0800

    java spi

--- app/src/main/java/com/feng/advance/design/FServiceLoader.java
+++ app/src/main/java/com/feng/advance/design/FServiceLoader.java
@@ -1,14 +1,16 @@
 package com.feng.advance.design;
 
 import com.feng.advance.copy2creat.TopUtil;
-import com.feng.advance.copy2creat.okhttp.Intercept;
+import com.feng.rounte_annotation.IAppInit;
+import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class FServiceLoader {
     public static void load(){
-        ServiceLoader<Intercept> loader = ServiceLoader.load(Intercept.class);
-        while (loader.iterator().hasNext()){
-            Intercept service = loader.iterator().next();
+        ServiceLoader<IAppInit> loader = ServiceLoader.load(IAppInit.class);
+        Iterator<IAppInit> iterator = loader.iterator();
+        while (iterator.hasNext()){
+            IAppInit service = iterator.next();
             TopUtil.print(service);
         }
     }
